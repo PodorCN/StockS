c82656b8907270241ca64c810859a16a4f946890
PodorCN <dev@podor.org>
2021-05-15 10:36:10 +0800

    ww

--- sqlConnect.py
+++ sqlConnect.py
@@ -13,7 +13,6 @@ ssh_passwd = '3-qJ{ov_LH324aww'
 ssh_port = 22
 sql_ip = '1.1.1.1.1'
 
-
 with SSHTunnelForwarder(
         (ssh_host, ssh_port),
         ssh_username=ssh_user,
@@ -21,18 +20,18 @@ with SSHTunnelForwarder(
         remote_bind_address=(sql_hostname, sql_port)) as tunnel:
     conn = mysql.connector.connect(host='127.0.0.1', user=sql_username,
             passwd=sql_password, db=sql_main_database,
-            port=tunnel.local_bind_port)    
+            port=tunnel.local_bind_port)
 
     mycursor = conn.cursor()
 
 
-    sql_command = """CREATE TABLE emp2 ( 
-    staff_number INTEGER PRIMARY KEY, 
-    fname VARCHAR(20), 
-    lname VARCHAR(30), 
-    gender CHAR(1), 
+    sql_command = """CREATE TABLE emp2 (
+    staff_number INTEGER PRIMARY KEY,
+    fname VARCHAR(20),
+    lname VARCHAR(30),
+    gender CHAR(1),
     joining DATE);"""
-  
+
 
     mycursor.execute(sql_command)
     sql_command = """INSERT INTO emp VALUES (23, "Rishabh", "Bansal", "M", "2014-03-28");"""
